72b63c2cebfa011f7f57da5c6aeb93fc3c7f7d8e
ciscoff <ciscoff@ya.ru>
2021-05-31 08:44:50 +0300

    +

--- app/src/main/java/barabudev/izitest/presentation/MainViewModelImpl.kt
+++ app/src/main/java/barabudev/izitest/presentation/MainViewModelImpl.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.*
 import barabudev.izitest.usecase.FetchUseCase
 import barabudev.izitest.usecase.WriteUseCase
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -13,8 +14,25 @@ class MainViewModelImpl(
     private val writeUseCase: WriteUseCase
 ) : ViewModel(), MainViewModel {
 
+    val liveData = liveData<String> {
+        while (true) {
+            delay(1000)
+            emit("work")
+        }
+    }
+
     // TODO Проверить в каком потоке Activity будет получать
-    val cache = liveData(Dispatchers.IO) {
+    /**
+     * Вот тут написано, что emit - это suspend и она останавливается ожидая
+     * когда её аргумент попадет в Main-поток. Получается, блок кода может
+     * работать на любом диспетчере, но результат все равно попадет в Main.
+     * https://developer.android.com/topic/libraries/architecture/coroutines
+     * 'Each emit() call suspends the execution of the block until the LiveData
+     * value is set on the main thread.'
+     *
+     * TODO - проверить !!!
+     */
+    val cache = liveData(viewModelScope.coroutineContext + Dispatchers.IO) {
         ('a'..'z').forEach { emit("'$it' in thread ${Thread.currentThread().name}") }
     }
 
