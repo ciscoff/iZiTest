70b91f53c77018299c33e4edffcde55cbb48f6dd
ciscoff <ciscoff@ya.ru>
2021-06-28 09:56:55 +0300

    +

--- app/src/test/java/barabudev/izitest/GwtEduTest.kt
+++ app/src/test/java/barabudev/izitest/GwtEduTest.kt
@@ -1,4 +1,26 @@
 package barabudev.izitest
 
+import android.content.Context
+import io.mockk.*
+import junit.framework.Assert.assertEquals
+import junit.framework.Assert.assertTrue
+import org.jetbrains.annotations.NotNull
+import org.junit.Before
+import org.junit.Test
+import org.mockito.ArgumentMatchers.*
+
 class GwtEduTest {
+
+    private val message = "hello, dolly"
+
+
+    @Test
+    fun `any clause is given section`() {
+        val mockkContext = mockk<Context>()
+
+        every { mockkContext.getString(anyInt()) } returns message
+        val result = mockkContext.getString(eq(5))
+
+        assertEquals(message, result)
+    }
 }
